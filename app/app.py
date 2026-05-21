ba0ce425dddefc13889162576c7e3a2afcb602c7
osogato180 <osogato180@gmail.com>
2026-05-20 20:33:27 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from pathlib import Path
 
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+st.title("Subida de archivos")
+
+uploaded_file = st.file_uploader("Selecciona un archivo")
+
+if uploaded_file is not None:
+    file_path = UPLOAD_DIR / uploaded_file.name
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success(f"Archivo guardado en: {file_path}")
